22ed94011be7e27f37904fb46f5f6c87bf66127a
HIPERION2021 <88901469+HIPERION2021@users.noreply.github.com>
2025-04-23 13:45:03 -0400

    Update tools.py

--- client/tools.py
+++ client/tools.py
@@ -23,18 +23,6 @@ def load_token(path):
     else:
         return ''
 
-def generate_token(length=32):
-    token_bytes = os.urandom(length)
-    return base64.urlsafe_b64encode(token_bytes).decode()
-
-def hash_token(token, salt, iterations= 10000):
-
-    if isinstance(token, str):
-        token = token.encode('utf-8')
-    if isinstance(salt, str):
-        salt = salt.encode('utf-8')
-    digest = hashlib.pbkdf2_hmac('sha256', token, salt, iterations)
-    return digest.hex()
 
 def generate_key_pair(curve=ec.SECP256R1):
     private_key = ec.generate_private_key(curve())
